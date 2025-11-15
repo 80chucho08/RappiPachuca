f0d30fcf61bedf47830d05380f269967fda8e745
80chucho08 <chucho2004carbajal@gmail.com>
2025-11-14 23:03:01 -0600

    feat: archivos para servicio web

---
+++ servicioweb/servicioweb.php
@@ -0,0 +1,11 @@
+<?php
+  //SE COLOCA EL NOMBRE DE LA PÁGINA QUE CONTIENE LOS MÉTODOS DEL SERVICIO WEB
+  require_once 'clsservicios.php';
+
+  //SE HACE USO DEL PROTOCOLO SOAPSERVER PARA ESTABLECER LA CONEXIÓN CON EL HOSTING
+  $soap=new SoapServer(null,array('uri' => 'http://localhost/'));
+  //SE EJECUTA LA CLASE QUE CONTIENE LOS MÉTODOS
+  $soap->setClass('clsservicios');
+  //SE EJECUTA LA CLASE
+  $soap->handle();
+?>
